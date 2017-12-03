21ce2dd7e50d88f047ed2364f5dd0011c9b457a9
HeXiongjun <TonyHo.Profession@gmail.com>
2017-12-03 22:24:37 +0800

    Change URL to Local, change the gradle version

--- app/src/main/java/com/github/nkzawa/socketio/androidchat/Constants.java
+++ app/src/main/java/com/github/nkzawa/socketio/androidchat/Constants.java
@@ -1,5 +1,6 @@
 package com.github.nkzawa.socketio.androidchat;
 
 public class Constants {
-    public static final String CHAT_SERVER_URL = "https://socket-io-chat.now.sh/";
+    //public static final String CHAT_SERVER_URL = "https://socket-io-chat.now.sh/";
+    public static final String CHAT_SERVER_URL = "http://192.168.1.100:3001";
 }
